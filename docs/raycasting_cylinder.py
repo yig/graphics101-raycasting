aefc237d849249adba3af69b06d24e8afaae8883
Yotam Gingold <yotam@yotamgingold.com>
2018-09-21 01:13:56 -0400

    Update raycasting_cylinder.py
    Updated pseudocode

--- docs/raycasting_cylinder.py
+++ docs/raycasting_cylinder.py
@@ -13,16 +13,20 @@ def intersect_cylinder( p, d ):
     
     ## z = 0 plane: F( x,y,z ) = -z
     t = -p.z / d.z
-    q = p + t * d
-    if q.x^2 + q.y^2 < 1:
-        ts.append( ( t, ( 0, 0, -1 ) )
+    if t >= 0:
+        q = p + t * d
+        if q.x^2 + q.y^2 < 1:
+            n = ( 0, 0, -1 )
+            ts.append( ( t, n ) )
     
     
     ## z = 1 plane: F( x,y,z ) = z - 1
     t = ( 1 - p.z ) / d.z
-    q = p + t * d
-    if q.x^2 + q.y^2 < 1:
-        ts.append( ( t, ( 0, 0, 1 ) )
+    if t >= 0:
+        q = p + t * d
+        if q.x^2 + q.y^2 < 1:
+            n = ( 0, 0, -1 )
+            ts.append( ( t, n ) )
     
     
     ## sides of the cylinder: F( x,y,z ) = x^2 + y^2 - 1
@@ -36,13 +40,17 @@ def intersect_cylinder( p, d ):
     radical = b^2 - 4*a*c
     if radical >= 0:
         t = ( -b + sqrt( radical ) )/( 2*a )
-        q = p + t * d
-        if 0 < q.z < 1: ts.append( ( t, ( 2*q.x, 2*q.y, 0 ) ) )
+        if t >= 0:
+            q = p + t * d
+            n = ( 2*q.x, 2*q.y, 0 )
+            if 0 < q.z < 1: ts.append( ( t, n ) )
         
         t = ( -b - sqrt( radical ) )/( 2*a )
-        q = p + t * d
-        if 0 < q.z < 1: ts.append( ( t, ( 2*q.x, 2*q.y, 0 ) ) )    
+        if t >= 0:
+            q = p + t * d
+            n = ( 2*q.x, 2*q.y, 0 )
+            if 0 < q.z < 1: ts.append( ( t, n ) )    
     
-    ts.sort()
-    for t, normal in ts:
-        if t >= 0: return t, normal
+    if ts.empty(): return None
+    
+    return min( ts )
